5bd5fdfd611567fa8ffee8605e759ae2b0e11e38
Sahil Kumar <sahilkumar71823@gmail.com>
2024-10-05 19:54:32 +0530

    committed!

--- let-var-const.js
+++ let-var-const.js
@@ -1,1 +1,28 @@
-console.log("Hello Javascript!")
+console.log("Hello Javascript!");
+const registrationNo=12217342;
+let studentEmail="sahil.12217342@lpu.in";
+var studentPass= "lihas123";
+
+studentCity="Patna";
+let studentState;
+
+
+//registrationNo=12225544; //const type, not allowed to change the value later
+
+console.log(registrationNo);
+studentEmail="sahil123@gmail.com";
+studentCity="Phagwara";
+studentPass="sahil123";
+
+console.table([registrationNo, studentEmail, studentPass, studentCity, studentState]);
+
+/*
+we can declare variables in two ways:
+1) let
+2) var
+
+
+we now do not prefer to use var type, (as there was some issue related to scope) due to issue in block scope and functional scope
+*/
+
+
