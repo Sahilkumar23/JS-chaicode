25bcd3e10cdf1ea65bf1262d85ac982f5971a352
Sahil Kumar <sahilkumar71823@gmail.com>
2024-10-21 20:27:40 +0530

    commit

--- 02-data-types.js
+++ 02-data-types.js
@@ -5,3 +5,32 @@
 
 
 //code readability
+
+console.log("Sahil");
+
+let name="Sahil"
+
+let age =18
+let isLoggedIn=false
+
+//primitive data types:
+
+/*number => 2 to power 53
+
+bigint
+string =>""
+boolean => true/false
+null => standalone value
+undefined => value is not assigned
+symbol => uniqueness 
+
+
+*OBJECT
+
+*/
+
+// console.log(typeof "Sahil");
+// console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
